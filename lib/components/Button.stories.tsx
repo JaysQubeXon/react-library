4f099f762e0dc2104698d1a067effe2aa27e8072
Noam Jacobson <noam.jacobson@protonmail.com>
2021-10-02 20:58:29 -0400

    Added variant to Button. Button story has style properties and button props.

--- lib/components/Button.stories.tsx
+++ lib/components/Button.stories.tsx
@@ -1,24 +1,35 @@
 import React from "react";
-import { Button } from "./Button";
+import { Button, ButtonProps } from "./Button";
 import { ComponentStory } from "@storybook/react";
 
 const story = {
   title: "Components/Button",
   component: Button,
   argTypes: {
-    type: "submit",
+    variant: "primary",
   },
 };
 
 export default story;
 
-const Template: ComponentStory<typeof Button> = (args) => {
+const Template: ComponentStory<React.FC<ButtonProps>> = (args) => {
   const { children, ...restProps } = args;
   return <Button {...restProps}>{children}</Button>;
 };
 
 export const Default = Template.bind({});
 Default.args = {
+  autoFocus: false,
+  disabled: false,
+  name: "default-button",
   type: "button",
+  value: "something",
+  variant: 'primary',
+  style: {
+      fontSize: 20,
+      fontWeight: 'initial',
+      borderRadius: 7,
+      padding: '0.5rem 1rem'
+  },
   children: "Click Me",
 };
--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -1,8 +1,48 @@
 import React, { ButtonHTMLAttributes } from "react";
 
-export interface ButtonProps extends ButtonHTMLAttributes<any> {}
+export interface ButtonProps extends ButtonHTMLAttributes<any> {
+  variant: "primary" | "secondary" | "warning" | "error";
+}
+
+const defaultStyles: React.CSSProperties = {
+  cursor: "pointer",
+};
+
+const disabledStyles: React.CSSProperties = {
+  cursor: "not-allowed",
+};
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { type, children } = props;
-  return <button type={type}>{children}</button>;
+  const { children, variant, style: styleProp, ...restProps } = props;
+  const styles = {
+    primary: {
+      backgroundColor: "lightblue",
+      border: "1px solid black",
+      color: "white",
+    },
+    secondary: {
+      backgroundColor: "transparent",
+      border: "1px solid black",
+      color: "black",
+    },
+    warning: {
+      backgroundColor: "yellow",
+      border: "1px solid black",
+      color: "black",
+    },
+    error: {
+      backgroundColor: "red",
+      border: "1px solid black",
+      color: "white",
+    },
+  }[variant];
+  const style = styles ?? {};
+  return (
+    <button
+      {...restProps}
+      style={{ ...defaultStyles, ...style, ...styleProp, ...(props.disabled ? disabledStyles : {}) }}
+    >
+      {children}
+    </button>
+  );
 };
